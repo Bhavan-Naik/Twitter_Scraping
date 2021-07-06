a001b274101eb1c6d4f26374cd74c2207348d8fe
Bhavan Naik <55109349+Bhavan-Naik@users.noreply.github.com>
2021-07-06 15:32:24 +0530

    Add files via upload

---
+++ twitter_shell.sh
@@ -0,0 +1,13 @@
+git clone --depth=1 https://github.com/twintproject/twint.git
+cd twint && pip3 install . -r requirements.txt
+cd ..
+pip3 install nest_asyncio
+pip3 install textblob
+pip3 install numpy
+pip3 install pandas
+
+
+pip3 install launchpadlib
+pip3 install tensorflow-io
+pip3 install kafka-python
+pip3 install pymongo
