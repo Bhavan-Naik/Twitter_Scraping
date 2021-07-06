86d4dfc3a10fdcaa355f24ec7ba79497f89c0fa9
Bhavan Naik <55109349+Bhavan-Naik@users.noreply.github.com>
2021-07-06 15:16:58 +0530

    Add files via upload

---
+++ Twitter_Historical/twitter_consumer.py
@@ -0,0 +1,34 @@
+from kafka import KafkaConsumer
+import json
+import os
+import datetime as dt
+from pymongo import MongoClient 
+
+try:
+    consumer = KafkaConsumer(
+            "tweet",
+            bootstrap_servers='localhost:9092',
+            auto_offset_reset='earliest',
+            group_id="twitter_consumer"
+        )
+        
+    time_script_run = dt.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')  
+    filename="{}_historical_output.json".format(time_script_run)
+
+    myclient = MongoClient("mongodb+srv://bazman:bhavan2000@twitter.fxec6.mongodb.net/test") 
+    db = myclient["Twitter_Historical"]
+    Collection = db[filename]
+
+    if __name__ == "__main__":
+        print('Starting the Consumer...')
+        print('Data-Scraping will take some time...')
+        for msg in consumer:        
+            new_tweet = {"$set":json.loads(msg.value)}
+            Collection.update_one(json.loads(msg.value), new_tweet, upsert=True)
+            #Collection.insert_one(json.loads(msg.value))
+            print("Tweet = {}".format(json.loads(msg.value)))
+        consumer.close()
+except:
+    consumer.close()
+    print("\r", end="")
+    exit(0)
